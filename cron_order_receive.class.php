755b25e33bdb633597fb68826c3a8b00c23d7c71
宋倩倩 <huangyuyuan@ecmoban.comshuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comqhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comqhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2019-03-28 15:09:26 +0800

    补充插件中遗漏的语言包

--- cron_order_receive.class.php
+++ cron_order_receive.class.php
@@ -73,7 +73,7 @@ class cron_order_receive extends CronAbstract
             ->get();
         
         foreach ($rows as $order) {
-            $order_operate->operate($order, 'receive', array('action_note' => '自动确认收货'));
+            $order_operate->operate($order, 'receive', array('action_note' => __('自动确认收货', 'cron_order_receive')));
         }
         unset($rows);
     }
